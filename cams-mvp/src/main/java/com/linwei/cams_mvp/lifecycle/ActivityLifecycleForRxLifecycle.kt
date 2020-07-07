07bbfa3b8a7e768f1878c2141e168f02341780c1
lw <linwei9605@gmail.com>
2020-07-07 11:48:26 +0800

    增加Cams-AAC、Cams-MVVP架构模块

--- cams-mvp/src/main/java/com/linwei/cams_mvp/lifecycle/ActivityLifecycleForRxLifecycle.kt
+++ cams-mvp/src/main/java/com/linwei/cams_mvp/lifecycle/ActivityLifecycleForRxLifecycle.kt
@@ -17,30 +17,23 @@ import javax.inject.Singleton
 @Singleton
 class ActivityLifecycleForRxLifecycle: ActivityLifecycleCallbacks {
     override fun onActivityPaused(activity: Activity) {
-        TODO("Not yet implemented")
     }
 
     override fun onActivityStarted(activity: Activity) {
-        TODO("Not yet implemented")
     }
 
     override fun onActivityDestroyed(activity: Activity) {
-        TODO("Not yet implemented")
     }
 
     override fun onActivitySaveInstanceState(activity: Activity, outState: Bundle) {
-        TODO("Not yet implemented")
     }
 
     override fun onActivityStopped(activity: Activity) {
-        TODO("Not yet implemented")
     }
 
     override fun onActivityCreated(activity: Activity, savedInstanceState: Bundle?) {
-        TODO("Not yet implemented")
     }
 
     override fun onActivityResumed(activity: Activity) {
-        TODO("Not yet implemented")
     }
 }
