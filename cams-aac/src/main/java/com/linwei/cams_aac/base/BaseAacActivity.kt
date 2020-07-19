1c1265d172a728ad4a3ae0f9b0a4bb0ecbc4315c
LW <linwei9605@gmail.com>
2020-07-19 23:17:19 +0800

    AAC模块Activity模块优化

---
+++ cams-aac/src/main/java/com/linwei/cams_aac/base/BaseAacActivity.kt
@@ -0,0 +1,22 @@
+package com.linwei.cams_aac.base
+
+import com.linwei.cams.base.activity.BaseActivity
+import com.linwei.cams.di.component.AppComponent
+/**
+ * ---------------------------------------------------------------------
+ * @Author: WeiShuai
+ * @Time: 2020/7/15
+ * @Contact: linwei9605@gmail.com
+ * @Github: https://github.com/WeiShuaiDev
+ * @Description:  `AAC` 架构 `Activity` 基类
+ *-----------------------------------------------------------------------
+ *
+ */
+abstract class BaseAacActivity : BaseActivity() {
+
+
+    override fun setUpActivityComponent(appComponent: AppComponent?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
